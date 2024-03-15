a8e1020f6809d446d7471129229c654e0ba95776
thelinuxfraud <151329771+thelinuxfraud@users.noreply.github.com>
2024-03-15 10:01:27 -0400

    Create autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+
+# Utilities
+run nm-applet &
+blueman-applet &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
+/usr/bin/dunst &
+run volumeicon &
+
+
+dbus-update-activation-enviornment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
